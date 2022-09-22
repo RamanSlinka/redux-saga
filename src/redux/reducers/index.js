8f0a988abb1e2782e2bd58bbe823b11ef3edf6a4
RamanSlinka <romuch1986@gmail.com>
2022-09-22 22:46:41 +0200

    Installing and configuring the environment. Created store.

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from "redux";
+
+
+const test = (state ={}, action) => {
+    return state
+}
+
+const reducer = combineReducers({
+    test
+});
+
+export default reducer;
