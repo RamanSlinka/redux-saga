6b88b87fd6876e359728f21ac54cb06acf6965da
RamanSlinka <romuch1986@gmail.com>
2022-09-25 18:02:03 +0200

    Redux integration(added actionCreator, constant file and counter reducer)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {decreaseCount, increaseCount} from "./redux/actions/actionCreator";
 
 
-const  App = ()=> {
+const App = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(store => store?.counter?.count)
 
-    const store = useSelector(state => state)
-    console.log(store)
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+
+    const handleIncrease = () => {
+        dispatch(increaseCount())
+    }
+    const handleDecrease = () => {
+        dispatch(decreaseCount())
+    }
+
+
+    return (
+        <div className="App">
+            <div>
+                <button onClick={handleIncrease}>+1</button>
+                <button onClick={handleDecrease}>-1</button>
+            </div>
+            <div> {count}</div>
+        </div>
+    );
 }
 
 export default App;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,11 @@
 import {combineReducers} from "redux";
+import counter from "./counter";
 
 
-const test = (state ={}, action) => {
-    return state
-}
+
 
 const reducer = combineReducers({
-    test
+    counter
 });
 
 export default reducer;
