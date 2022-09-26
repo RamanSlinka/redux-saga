aafdc46de649a61b01152d06ca6ea5ea11609c59
RamanSlinka <romuch1986@gmail.com>
2022-09-26 23:01:48 +0200

    Redux-Saga effects

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,14 +1,25 @@
-import { take }  from "@redux-saga/core/effects";
+import { take, takeEvery, takeLatest, takeLeading, select }  from "@redux-saga/core/effects";
 import {INCREASE_COUNT, DECREASE_COUNT} from "../constants";
 
 
+const delay = (time) => new Promise((resolve , reject) => {
+    setTimeout(resolve, time * 1000);
+})
+
 export function* workerSaga() {
+    const count = yield select(({counter}) => counter.count)
+
+    yield delay(2)
+    console.log(`request  ${count}`);
 
 }
 
 export function* watchClickSaga() {
-    yield take(INCREASE_COUNT);
-    console.log('request 1')
+    // yield takeEvery(INCREASE_COUNT,  workerSaga);
+
+    yield takeLatest(INCREASE_COUNT,  workerSaga);
+    yield takeLeading(INCREASE_COUNT,  workerSaga);
+
     yield take(DECREASE_COUNT);
     console.log('request 2')
 }
