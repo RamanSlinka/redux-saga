7c3d7c6f626de2c8e31187531041dfed1446f8cb
RamanSlinka <romuch1986@gmail.com>
2022-09-28 22:58:06 +0200

    Redux-Saga : Parallel Tasks

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,22 +1,30 @@
-import { takeEvery, put, call }  from "@redux-saga/core/effects";
-import {GET_LATEST_NEWS, SET_LATEST_NEWS} from "../constants";
-import {getLatestNews} from "../../api";
-import {setLatestNews} from "../actions/actionCreator"
-
-
+import {takeEvery, put, call, fork, all, race} from "@redux-saga/core/effects";
+import {GET_NEWS} from "../constants";
+import {getLatestNews, getPopularNews} from "../../api";
+import {setLatestNews, setPopularNews} from "../actions/actionCreator"
 
 
 export function* handleLatestNews() {
-const {hits} = yield call(getLatestNews, 'react');
+    const {hits} = yield call(getLatestNews, 'react');
     yield put(setLatestNews(hits));
+}
+
+export function* handlePopularNews() {
+    const {hits} = yield call(getPopularNews);
+    yield put(setPopularNews(hits));
+}
+
+export function* handleNews() {
+    yield fork(handleLatestNews)
+    yield fork(handlePopularNews)
 
 }
 
 export function* watchClickSaga() {
-    yield takeEvery(GET_LATEST_NEWS,  handleLatestNews);
+    yield takeEvery(GET_NEWS, handleNews);
 
 }
 
-export default  function* rootSaga () {
-      yield  watchClickSaga();
+export default function* rootSaga() {
+    yield  watchClickSaga();
 }
