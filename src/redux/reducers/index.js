15e0c5a9d609dc8a667549e396b1141b62833761
RamanSlinka <romuch1986@gmail.com>
2022-10-02 16:23:55 +0200

    Redux-Saga : Errors Handling, add error reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,10 @@
 import {combineReducers} from "redux";
-import counter from "./counter";
 import news from "./news";
-
-
+import errors from "./errors";
 
 
 const reducer = combineReducers({
-    counter,
-    news
+    news, errors
 });
 
 export default reducer;
